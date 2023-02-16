79b20f7c7fe1cde9b9855e9353a228493c4b4038
groothendrik <groothendrik.steyn@gmail.com>
2023-02-16 12:05:24 +0200

    Exersise 2.2 on variables.

---
+++ # 2.2.py
@@ -0,0 +1,5 @@
+# 2.2 Write a program that uses input to prompt a user for their name and then welcomes them. Note that input will pop up a dialog box. Enter Sarah in the pop-up box when you are prompted so your output will match the desired output.
+# The first exercise on chapter 2 of the py4e text book.
+
+name = input("Enter your name")
+print("Hello", name)
