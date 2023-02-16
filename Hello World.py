1d74232f0ed8e1d794bb817529b31ba545042b88
groothendrik <groothendrik.steyn@gmail.com>
2023-02-16 11:50:56 +0200

    First program of the textbook on py4e.com

---
+++ Hello World.py
@@ -0,0 +1,4 @@
+# This is the first program of the Python for Everybody text book.
+# Write a program that uses a print function to say 'hello world' as shown in 'Desired Output'.
+
+print("Hello world!")
